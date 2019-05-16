c48befcbef296e73e0020ae2532a069985ee492a
student-norinori <a18z3nn@aiit.ac.jp>
2019-05-16 08:43:06 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # => app/views/users/show.html.erb
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       redirect_to @user
+    else
+      flash[:success] = "Welcome to the Sample App!"
+      render 'new'
+    end
+  end
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+
 end
+
