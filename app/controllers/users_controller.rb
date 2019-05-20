2d350cfc05d8dd507669785299c69a4fb81b6c1b
student-norinori <a18z3nn@aiit.ac.jp>
2019-05-20 12:29:26 +0000

    Finish ch08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-       redirect_to @user
-    else
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
       render 'new'
     end
   end
